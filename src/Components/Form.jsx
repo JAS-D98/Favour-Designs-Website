15259bae4ca0aeaf5a8891594e9c565e8828589a
Jasper Ngunjiri <jasperwambugu@gmail.com>
2024-01-30 23:11:05 +0300

    made flex box changes to have flex direction be col for all screens

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -31,7 +31,7 @@ const Form = () => {
       <form
         method='post'
         onSubmit={handleSubmit}
-        className='flex mx-auto max-w-sm flex-col items-center gap-4 bg-slate-200 p-2 sm:p-4 rounded-lg'
+        className='flex mx-auto max-w-md flex-col items-center gap-4 bg-slate-200 p-2 sm:p-4 rounded-lg'
       >
         <h1 className='text-2xl text-slate-600'>
           Contact <span className='text-firebrick'>Here</span>
@@ -47,19 +47,19 @@ const Form = () => {
           </div>
         )}
         <div className='flex flex-col gap-2 w-full'>
-        <div className='flex justify-start sm:items-center  gap-2 flex-col sm:flex-row'>
+        <div className='flex justify-start sm:items-center  gap-2 flex-col'>
                 <label htmlFor="name" className='text-xl text-slate-600'>Name:</label>
                 <input type="text" name="name" onChange={handleChange} id="" value={data.name} placeholder='Enter Your Name Here' className='w-full p-2 px-2 rounded-full bg-firebrick text-white outline-none'/>
             </div>
-            <div className='flex justify-start sm:items-center gap-2 flex-col sm:flex-row'>
+            <div className='flex justify-start sm:items-center gap-2 flex-col'>
                 <label htmlFor="email" className='text-xl text-slate-600'>Email:</label>
                 <input type="email" name="email" onChange={handleChange} id="" value={data.email} placeholder='Enter Your Email Address' className='w-full p-2 px-2 rounded-full bg-firebrick text-white outline-none'/>
             </div>
-            <div className='flex justify-start sm:items-center gap-2 flex-col sm:flex-row'>
+            <div className='flex justify-start sm:items-center gap-2 flex-col'>
                 <label htmlFor="email" className='text-xl text-slate-600'>Phone:</label>
             <input type="phone" name="phone" onChange={handleChange} id="" value={data.phone} placeholder='Enter your Phone Number' className='w-full p-2 px-2 rounded-full bg-firebrick text-white outline-none'/>
             </div>
-            <div className='flex flex-col sm:flex-row gap-2'>
+            <div className='flex flex-col gap-2'>
             <label htmlFor="message" className='text-xl text-slate-600'>Email:</label>   
             <textarea name='message' id='' cols="30" rows="10" onChange={handleChange} value={data.message} placeholder='Enter Your Message Here...' className='bg-firebrick  text-white outline-none w-full rounded-md p-4'/>
             </div>

3b4a48a0431fac3b8190e13149c9eed08cedc5cd
Jasper Ngunjiri <jasperwambugu@gmail.com>
2024-01-30 23:32:03 +0300

    adjusted margin top

--- src/Components/DesignGallery.jsx
+++ src/Components/DesignGallery.jsx
@@ -3,7 +3,7 @@ import DesignsCard from './DesignsCard';
 
 const DesignGallery = ({ designs, searchTerm }) => {
   return (
-    <div className="mt-8 sm:mt-6 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-6 w-full">
+    <div className="mt-10 sm:mt-10 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-6 w-full">
       {designs
         .filter((design) => {
           if (searchTerm === '') {

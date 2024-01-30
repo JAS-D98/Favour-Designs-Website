0c73bf92c084e853ae636ca4ee5ee4acd2f1362c
Jasper Ngunjiri <jasperwambugu@gmail.com>
2024-01-30 23:29:49 +0300

    adjusted image width and height

--- src/Components/DesignsCard.jsx
+++ src/Components/DesignsCard.jsx
@@ -1,8 +1,8 @@
 const DesignsCard = ({ design }) => {
   return (
-    <div className="flex flex-1 items-center flex-col sm:w-full rounded-3xl w-full sm:max-w-[350px] shadow-xl pb-3 mt-6">
-      <div className="flex items-center justify-center h-[200px] sm:h-[300px] w-full">
-        <img src={design.imgURL} alt={design.name} className="h-full w-full object-cover" />
+    <div className="flex flex-1 items-center flex-col sm:w-full rounded-3xl w-full sm:max-w-[400px] shadow-xl pb-3 mt-6">
+      <div className="flex items-center justify-center h-[400px] sm:h-[400px] w-full">
+        <img src={design.imgURL} alt={design.name} className="h-full w-full object-contain" />
       </div>
       <div className="mt-4 flex-col flex justify-start gap-1 items-center">
         <h3 className="text-slate-600 text-center mt-1 text-lg sm:text-xl">{design.name}</h3>

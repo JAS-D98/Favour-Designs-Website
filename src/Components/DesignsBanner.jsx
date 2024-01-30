ddcbc894de6ff7b26dc0021be9ecdbe97ab67d6f
Jasper Ngunjiri <jasperwambugu@gmail.com>
2024-01-30 23:38:58 +0300

    added a margin auto to center the buttons

--- src/Components/DesignsBanner.jsx
+++ src/Components/DesignsBanner.jsx
@@ -10,7 +10,7 @@ const DesignsBanner = ({ searchTerm, setSearchTerm, handleFilterChange }) => {
       <Banner sentence={Sentence} />
       <div className='flex justify-between flex-col-reverse sm:flex-row-reverse gap-2 sm:gap-6 text-[16px] text-slate-600'>
         <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm}/>
-        <div className='flex gap-2 items-center justify-center flex-wrap sm:gap-12'>
+        <div className='flex gap-2 items-center justify-center flex-wrap sm:gap-12 mx-auto'>
           <button className='bg-gradient-to-t from-black to-red-600 font-[16px] min-w-[90px] hover:opacity-80 text-white px-2 py-0 sm:py-1 rounded-md' onClick={() => handleFilterChange('all')}>All</button>
           <button className='bg-gradient-to-t from-black to-red-600 font-[16px] min-w-[90px] hover:opacity-80 text-white px-2 py-0 sm:py-1 rounded-md' onClick={() => handleFilterChange('Men')}>Men</button>
           <button className='bg-gradient-to-t from-black to-red-600 font-[16px] min-w-[90px] hover:opacity-80 text-white px-2 py-0 sm:py-1 rounded-md' onClick={() => handleFilterChange('Women')}>Women</button>
